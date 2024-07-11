6f82ab1330ebbf46fd75c291be785c61044d2f9b
code-wizard-18 <codewizard18@gmail.com>
2024-07-11 19:09:31 +0530

    added the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-// import Footer from './components/Footer';
-import Home from './pages/Home/Home';
-// import Services from './pages/Services';
+import Home from './pages/Home/Home.jsx';
 import './App.css';
-import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer.jsx';
 import Services from './components/Services/Services.jsx';
 import Products from './components/Products/Products.jsx';
 import AboutUs from './components/AboutUs/AboutUs.jsx'
