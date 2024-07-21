cbde368fd7b715272d1c3e44cfeceeecfae6fc0f
code-wizard-18 <codewizard18@gmail.com>
2024-07-21 06:39:20 +0530

    Added th

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React , {useEffect,useState} from 'react';
 import './ContactUs.css'; 
-
+import sendEmail from '../EmailHelper/sendEmail';
 const ContactUs = () => {
+    const [formData , setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        phone: '',
+        message: ''
+    })
+    const handleChange = (e, type) => {
+        console.log(e.target.value)
+        setFormData({ ...formData, [type]: e.target.value })
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        sendEmail(formData)
+        setFormData({
+            name: '',
+            email: '',
+            phone: '',
+            subject: '',
+            message: ''
+        })
+    }
+    
+    useEffect(() => {
+        // unmount component
+        console.log(formData)
+        return () => {
+            setFormData({
+                name: '',
+                email: '',
+                phone: '',
+                subject: '',
+                message: ''
+            })
+        }
+    }, [])
+
     return (
         <div className='contact-us-container'>
             {/* map location section */}
@@ -17,26 +55,26 @@ const ContactUs = () => {
                     <div className='contact-us-input-container'>
                     <div className='input-container'>
                         <label htmlFor="name">Name</label>
-                        <input type="text" placeholder='Name' />
+                        <input type="text" name ="name" placeholder='Name' onChange={(e)=> handleChange(e, 'name')} value = {formData.name} />
                     </div>
                     <div className='input-container'>
                         <label htmlFor="email">Email</label>
-                        <input type="text" placeholder='Email' />
+                        <input type="text"  name ="email" placeholder='Email' onChange={(e)=> handleChange(e, 'email')} value={formData.email} />
                     </div>
                     <div className='input-container'>
                         <label htmlFor="phone">Phone</label>
-                        <input type="text" placeholder='Phone' />
+                        <input type="text" name ="phone" placeholder='Phone' onChange={(e)=> handleChange(e, 'phone')} value={formData.phone} />
                     </div>
                     <div className='input-container'>
                         <label htmlFor="phone">Subject</label>
-                        <input type="text" placeholder='Subject' />
+                        <input type="text" name ="subject" placeholder='Subject' onChange={(e)=> handleChange(e, 'subject')} value={formData.subject} />
                     </div>
                     <div className='input-container'>
                         <label htmlFor="message">Message</label>
-                        <textarea placeholder='Message'></textarea>
+                        <textarea placeholder='Message' name="message" onChange={(e)=> handleChange(e, 'message')} value={formData.message} ></textarea>
                     </div>
                     </div>
-                    <button className='contact-us-button'>Submit</button>
+                    <button className='contact-us-button' onClick={(e)=> handleClick(e)} >Submit</button>
                 </div>
             </div>
         </div>
