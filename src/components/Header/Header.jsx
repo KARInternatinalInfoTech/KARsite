808c7dbb4d5f10875270964f9e4efe8edf9a96cc
code-wizard-18 <codewizard18@gmail.com>
2024-07-21 08:29:15 +0530

    updated the  responsive changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,16 @@ const Header = () => {
         <img src='/images/logo.svg' width={25} height={25} alt="Logo" />
       </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+      <svg width="28" height="34" viewBox="0 0 28 34" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+    <rect width="28" height="34" fill="url(#pattern0_203_2)" />
+    <defs>
+      <pattern id="pattern0_203_2" patternContentUnits="objectBoundingBox" width="1" height="1">
+        <use xlinkHref="#image0_203_2" transform="matrix(0.0111111 0 0 0.00915033 0 0.0882353)" />
+      </pattern>
+      <image id="image0_203_2" width="90" height="90" xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFoAAABaCAYAAAA4qEECAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAqklEQVR4nO3ZPQrCQBSF0WCny/cHs0q10faKaKVgmIE8A56zgClu8ab4hgEAAAAAAACA30mySXJIcglTzkn2SdY9Q4+Tz/NubB15leT28QxTro/tDL20oZ2Obse2A/08H+sku9eh57tTkm3XZwgAAAAA8OfE2SbibDFxtog4W0ScLSLOzkycBQAAAADoJM42EWeLibNFxNki4mwRcXZm4iwAAAAAAADAsAB337XoqI2Y7e8AAAAASUVORK5CYII=" />
+    </defs>
+  </svg>
+
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
